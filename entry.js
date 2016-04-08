96c8129c41e1c708b85cf7be8cc3998bdf4919ed
chenjunsheng <641232582@qq.com>
2016-04-08 11:16:11 +0800

    css loader

---
+++ entry.js
@@ -0,0 +1,4 @@
+require('./first.js');
+require('!style!css!./style.css');
+
+document.getElementById('app').innerHTML = 'this is the first webpack demo';
---
+++ first.js
@@ -0,0 +1,3 @@
+var h2 = document.createElement('h2');
+h2.innerHTML = 'this is the second demo';
+document.body.appendChild(h2);
